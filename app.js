0b4e165acd10e6c7b89f0d9ba81065ad13fc7f46
haydher <haider0612@gmail.com>
2022-01-30 20:18:31 -0500

    added nodemailer

--- app.js
+++ app.js
@@ -1,17 +1,61 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+
+require("dotenv").config();
+
 const app = express();
 
 app.use(cors());
-// body parser
+
 app.use(bodyParser.json());
 
 app.post("/", (req, res) => {
- console.log("req.body", req.body);
- res.send({ status: 200, message: "Form submitted successfully" });
+ const transporter = nodemailer.createTransport({
+  host: process.env.SENDER_EMAIL_HOST,
+  port: parseInt(process.env.SENDER_EMAIL_PORT),
+  auth: {
+   user: process.env.SENDER_EMAIL_ADDRESS,
+   pass: process.env.SENDER_EMAIL_PASSWORD,
+  },
+ });
+
+ const textMsg = `
+ New Message!
+ From:
+ Full Name: ${req.body.fullName}
+ Email: ${req.body.email}
+ Message: ${req.body.message}
+ `;
+
+ const htmlMsg = `
+ <h3>New Message!</h3>
+ <h4>From:</h4>
+ <p><b>Full Name:</b>    ${req.body.fullName}</p>
+ <p><b>Email:</b>    ${req.body.email}</p>
+ <br>
+ <p><b>Message:</b><br><br> ${req.body.message}</p>
+ `;
+
+ const options = {
+  from: `"${req.body.fullName}" <${req.body.email}`,
+  to: process.env.TO_EMAIL_ADDRESS,
+  subject: "Message from Portfolio",
+  text: textMsg,
+  html: htmlMsg,
+ };
+
+ transporter.sendMail(options, (err, info) => {
+  if (err) {
+   console.log("Error sending a message from the contact form.", err);
+   return res.send({ status: 500, message: "Unable to send the message." });
+  }
+  console.log("sent mail", info.response);
+  return res.send({ status: 200, message: "Message sent successfully." });
+ });
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
