6b99d65c59fad8b9d7e2bab1eebe402faea8d92d
Haydher <haider0612@gmail.com>
2022-02-06 20:09:14 -0500

    changed base url

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send({ status: 200 }));
+app.get("/turnServerOn", (req, res) => res.send({ status: 200 }));
 
 app.post("/", (req, res) => {
  const transporter = nodemailer.createTransport({
